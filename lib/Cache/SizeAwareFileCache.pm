28df9d129fc984ec48ccfbb7bbd03a774b1f3c75
dclinton <dclinton@fe4db14e-1138-0410-ae3a-bd28db37d31a>
2001-02-15 16:39:55 +0000

    * overloaded the _store method to dynamically check whether or not   the cache needs to be resized to stay under the max_size * implemented the reduce_size( $new_size ) method to dynamically   resize the cache * implemented the _Choose_Identifier_To_Remove routine to select either   the next expiring identifier or the least recently accessed identifier,   in that order * implemented the _Find_Next_Expiring_Identifier routine * implemented the _Find_Least_Recently_Accessed_Identifier routine * implemented the _Restore_Object_Without_Time_Modication routine, although   this should perhaps be moved into CacheUtils

--- lib/Cache/SizeAwareFileCache.pm
+++ lib/Cache/SizeAwareFileCache.pm
@@ -1,5 +1,5 @@
 ######################################################################
-# $Id: FileCache.pm,v 1.2 2001/02/13 02:32:03 dclinton Exp $
+# $Id: SizeAwareFileCache.pm,v 1.1 2001/02/15 15:49:49 dclinton Exp $
 # Copyright (C) 2001 DeWitt Clinton  All Rights Reserved
 #
 # Software distributed under the License is distributed on an "AS
@@ -13,8 +13,13 @@ package Cache::SizeAwareFileCache;
 use strict;
 use vars qw( @ISA @EXPORT_OK $NO_MAX_SIZE );
 use Cache::Cache qw( $EXPIRES_NEVER $SUCCESS $FAILURE $TRUE $FALSE );
+use Cache::CacheUtils qw ( Make_Path
+                           Recursively_List_Files
+                           Read_File_Without_Time_Modification
+                           Write_File );
 use Cache::FileCache;
 use Carp;
+use Data::Dumper;
 use Exporter;
 
 @ISA = qw ( Cache::FileCache );
@@ -44,6 +49,196 @@ sub new
     croak( "Couldn't initialize Cache::SizeAwareFileCache" );
 
   return $self;
+}
+
+
+
+
+sub _store
+{
+  my ( $self, $unique_key, $object ) = @_;
+
+  $unique_key or
+    croak( "unique_key required" );
+
+  my $object_path = $self->_build_object_path( $unique_key ) or
+    croak( "Couldn't build object path" );
+
+  my $data_dumper = new Data::Dumper( [$object] );
+
+  $data_dumper->Deepcopy( 1 );
+
+  my $object_dump = $data_dumper->Dump( );
+
+  Make_Path( $object_path ) or
+    croak( "Couldn't make path: $object_path" );
+
+  my $max_size = $self->get_max_size();
+
+  if ( $max_size != $NO_MAX_SIZE )
+  {
+    my $new_size = $max_size - length $object_dump;
+
+    $new_size = 0 if $new_size < 0;
+
+    $self->reduce_size( $new_size );
+  }
+
+  Write_File( $object_path, \$object_dump ) or
+    croak( "Couldn't write file $object_path" );
+
+  return $SUCCESS;
+}
+
+
+sub reduce_size
+{
+  my ( $self, $new_size ) = @_;
+
+  $new_size >= 0 or
+    croak("size >= 0 required");
+
+  my $namespace_path = $self->_build_namespace_path( ) or
+    croak( "Couldn't build namespace path" );
+
+  while ( $self->size() > $new_size )
+  {
+    my $identifier_to_remove = _Choose_Identifier_To_Remove( $namespace_path );
+
+    if ( not defined $identifier_to_remove )
+    {
+      warn("Couldn't reduce size to $new_size\n");
+
+      return $FAILURE;
+    }
+
+    $self->remove( $identifier_to_remove ) or
+      croak( "Couldn't remove identifier $identifier_to_remove" );
+  }
+
+  return $SUCCESS;
+}
+
+
+sub _Choose_Identifier_To_Remove
+{
+  my ( $namespace_path ) = @_;
+
+  defined( $namespace_path ) or
+    croak( "namespace_path required" );
+
+  my $next_expiring_indentifier = 
+    _Find_Next_Expiring_Identifier( $namespace_path );
+
+  if ( defined $next_expiring_indentifier )
+  {
+    return $next_expiring_indentifier;
+  }
+
+  my $least_recently_accessed_identifier =
+    _Find_Least_Recently_Accessed_Identifier( $namespace_path );
+
+  if ( defined $least_recently_accessed_identifier )
+  {
+    return $least_recently_accessed_identifier;
+  }
+
+  return undef;
+}
+
+
+
+sub _Find_Next_Expiring_Identifier
+{
+  my ( $namespace_path ) = @_;
+
+  defined( $namespace_path ) or
+    croak( "namespace_path required" );
+
+  my $next_expiring_indentifier = undef;
+
+  my $next_expires_at = undef;
+
+  my @filenames = Recursively_List_Files( $namespace_path );
+
+  foreach my $filename ( @filenames )
+  {
+    my $object = _Restore_Object_Without_Time_Modication( $filename ) or
+      next;
+
+    my $expires_at = $object->get_expires_at( );
+
+    next if $expires_at = $EXPIRES_NEVER;
+
+    if ( ( not defined $next_expires_at ) or
+         ( $expires_at < $next_expires_at ) )
+    {
+      $next_expires_at = $expires_at;
+
+      $next_expiring_indentifier = $object->get_identifier( ) or
+        croak( "Couldn't get identifier" );
+    }
+  }
+
+  return $next_expiring_indentifier;
+}
+
+
+sub _Find_Least_Recently_Accessed_Identifier
+{
+  my ( $namespace_path ) = @_;
+
+  defined( $namespace_path ) or
+    croak( "namespace_path required" );
+
+  my $least_recently_accessed_identifier = undef;
+
+  my $least_recent_access_time = undef;
+
+  my @filenames = Recursively_List_Files( $namespace_path );
+
+  foreach my $filename ( @filenames )
+  {
+    my $last_access_time = ( stat( $filename ) )[8];
+
+    if ( ( not defined $least_recent_access_time ) or
+         ( $least_recent_access_time < $last_access_time ) )
+    {
+      my $object = _Restore_Object_Without_Time_Modication( $filename ) or
+        next;
+
+      $least_recent_access_time = $last_access_time;
+
+      $least_recently_accessed_identifier = $object->identifier( ) or
+        croak( "Couldn't get identifier" );
+    }
+  }
+
+  return $least_recently_accessed_identifier;
+}
+
+
+sub _Restore_Object_Without_Time_Modication
+{
+  my ( $filename ) = @_;
+
+  defined( $filename ) or
+    croak( "filename required" );
+
+  my $object_dump_ref = Read_File_Without_Time_Modification( $filename ) or
+    return undef;
+
+  no strict 'refs';
+
+  my $VAR1;
+
+  eval $$object_dump_ref;
+
+  my $object = $VAR1;
+
+  use strict;
+
+  return $object;
 }
 
 
